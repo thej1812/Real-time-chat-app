35f706001d7cb5f1842d3d958db019e17decc122
thej1812 <thejashwini1800@gmail.com>
2026-01-11 22:07:48 +0530

    j

--- frontend/src/components/JoinRoom.jsx
+++ frontend/src/components/JoinRoom.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import './Joinroom.css'
+import './joinroom.css'
 export default function JoinRoom() {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
